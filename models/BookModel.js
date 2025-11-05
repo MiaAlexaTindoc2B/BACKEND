467f3036ae15358567f45e39104064d925bb853d
MiaAlexaTindoc2B <23-14633@g.batstate-u.edu.ph>
2025-11-05 16:58:51 +0800

    CRUD on Book Table

--- models/BookModel.js
+++ models/BookModel.js
@@ -1,6 +1,31 @@
+import { stat } from 'fs';
 import pool from './db.js';
 
 export const getBooks = async () =>{
     const [rows] = await pool.query("SELECT * FROM tbl_book")
     return rows;
 }
+
+export const insertBook = async (title, genre, status) => {
+    const [result] = await pool.query(
+        "INSERT INTO tbl_book (title, genre, status) VALUES (?, ?, ?)", 
+        [title, genre, status]
+    );
+    return result.insertId;
+}
+
+export const updateBook = async (title, genre, status, bookId) => {
+    const [result] = await pool.query(
+        "UPDATE tbl_book SET title = ?, genre = ?, status = ? WHERE id = ?",
+        [title, genre, status, bookId]
+    );
+    return result.affectedRows;
+}   
+
+export const deleteBook = async (bookId) => {
+    const [result] = await pool.query(
+        "DELETE FROM tbl_book WHERE id = ?",
+        [bookId]
+    );
+    return result.affectedRows;
+}    
